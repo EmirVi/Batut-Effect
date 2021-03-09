e3b959afb9db54b24c32e2dda2a8a85fe3a63b3e
EmirVi <70579571+EmirVi@users.noreply.github.com>
2021-03-09 14:15:40 +0400

    update

---
+++ FinalBackendServer/Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Networking;
+using UnityEngine.UI;
+
+public class ConnectionManager : MonoBehaviour
+{
+    public InputField r_username;
+    public InputField r_password;
+    Coroutine registrationCoroutine;
+
+    public void SubmitRegistrationForm()
+    {
+
+    }
+
+    IEnumerator SubmitRegisterForm(string username, string password)
+    {
+
+    }
+}
---
+++ FinalBackendServer/Assets/Scripts/WebServices.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebServices : MonoBehaviour
+{
+    static readonly string mainURL = "http://localhost:3000/";
+}
